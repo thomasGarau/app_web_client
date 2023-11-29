78de07105483040df10fabcaf65a96551f35076b
thomasGarau <thomas.garau8@gmail.com>
2023-11-29 13:11:08 +0100

    changemt de la methode pour requet logout

--- src/connexion/UserAPI.js
+++ src/connexion/UserAPI.js
@@ -6,7 +6,7 @@ export const Authenticate = async (user, password) => {
         username:user,
         password
     })
-    .then(response => {return response.data})
+    .then(response => {console.log(response.data);return response.data})
     .catch(error => {console.log(error); throw error;})
 }
 
