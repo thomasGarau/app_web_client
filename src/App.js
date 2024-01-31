bd49fc022bc552a134164f6bc1379d07c2a411c1
anthony marchiselli <anthony.mar2a@gmail.com>
2024-01-31 15:45:44 +0100

    [ADD] Ajout d'un fichier css pour la page principale

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Remplacez "Switch" par "Routes"
 import PrivateRoute from './services/PrivateRoutes';
-import Principal from './Principal';
-import Connexion from './connexion/Connexion'
-import Register from './connexion/Register'
-import Liste_ue from './liste_ue/Liste_ue'
-import Carte_mental from './carte_mental/Carte_mental'
+import Principal from './principal/Principal';
+import Connexion from './connexion/Connexion';
+import Register from './connexion/Register';
+import Liste_ue from './liste_ue/Liste_ue';
+import Carte_mental from './carte_mental/Carte_mental';
 import SecurePage from './secure_page/securePage';
 function App() {
   return (
--- src/principal/Principal.js
+++ src/principal/Principal.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './principal.css';
+import "@fontsource/nanum-pen-script";
 
 function Accueil() {
   const navigate = useNavigate();
@@ -13,11 +15,11 @@ function Accueil() {
   };
 
   return (
-    <div>
-      <h1>Page d'accueil</h1>
-      <p>Bienvenue sur la page d'accueil de mon application.</p>
-      <button onClick={handleRegisterClick}>Inscription</button>
-      <button onClick={handleConnexionClick}>Connexion</button>
+    <div className='style_background'>
+      <div className='container1_style'>
+        <button className='button1' onClick={handleRegisterClick}>Inscription</button>
+        <button onClick={handleConnexionClick}>Connexion</button>
+      </div>
     </div>
   );
 }
