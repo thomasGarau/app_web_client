8f034ac54d2251e111400399905bcc1c15d0f781
MarieEveille <eveille.marie29@gmail.com>
2024-02-02 16:09:59 +0100

    [Delete] Liste ue

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import PrivateRoute2 from './services/PrivateRoutes2';
 import Principal from './principal/Principal';
 import Connexion from './connexion/Connexion';
 import Register from './connexion/Register';
-import Liste_ue from './liste_ue/Liste_ue';
 import Carte_mental from './carte_mental/Carte_mental';
 import TestHeader from './test/TestHeader'
 import SecurePage from './secure_page/securePage';
@@ -16,7 +15,6 @@ function App() {
       <Routes> {/* Utilisez la composante "Routes" au lieu de "Switch" */}
         <Route path="/connexion" element={<Connexion />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/liste_ue" element={<PrivateRoute><Liste_ue /></PrivateRoute>} /> 
         <Route path="/carte_mental" element={<PrivateRoute><Carte_mental /></PrivateRoute>} />
         <Route path="/secure_page" element={<PrivateRoute><SecurePage /></PrivateRoute>} />*
         <Route path="/testHeader" element={<TestHeader />} /> 
