f5c0bc3483166b01aa63e4b8843f10c013a1a169
Vietco311 <ant.colin311@gmail.com>
2024-02-04 15:12:18 +0100

    [NEW] Desgin de la page Study

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Carte_mental from './carte_mental/Carte_mental';
 import TestHeader from './test/TestHeader'
 import SecurePage from './secure_page/securePage';
 import Profile from './profile/Profile';
+import Study from './STUDY/Study';
+
 function App() {
   return (
     <Router>
@@ -18,7 +20,8 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/liste_ue" element={<PrivateRoute><Liste_ue /></PrivateRoute>} /> 
         <Route path="/carte_mental" element={<PrivateRoute><Carte_mental /></PrivateRoute>} />
-        <Route path="/secure_page" element={<PrivateRoute><SecurePage /></PrivateRoute>} />*
+        <Route path="/etude" element={/* <PrivateRoute> */<Study/>/* </PrivateRoute> */}/>
+        <Route path="/secure_page" element={<PrivateRoute><SecurePage /></PrivateRoute>} />
         <Route path="/testHeader" element={<TestHeader />} /> 
         <Route path='/secure_page2' element={<PrivateRoute2><SecurePage /></PrivateRoute2>} />
         <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
---
+++ src/STUDY/Study.js
@@ -0,0 +1,28 @@
+//dependances
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Header from '../composent/Header';
+import './Study.css';
+
+function Study(){
+
+    const [cours, setCours] = useState("Lorem Ipsum");
+    const [texte, setTexte] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus semper leo sed magna tincidunt, nec suscipit sem ullamcorper. Duis at felis id turpis semper facilisis sed sed nulla. Donec consequat aliquet nulla, vel elementum erat luctus sit amet. Nunc pretium posuere ex nec posuere. Praesent sit amet nulla ac lacus accumsan fermentum sodales non tellus. Sed est sapien, tempus non rutrum at, porttitor placerat sem. Pellentesque sed hendrerit sem. Mauris lobortis sagittis augue, ac imperdiet dolor gravida quis. Phasellus sapien velit, scelerisque pretium odio quis, tincidunt facilisis lectus. Nulla sit amet vehicula tortor. Nam faucibus ex nisi, sed bibendum leo accumsan id.")
+    const [questions, setQuestions] = useState(["Une question?", "Deux question?"])
+
+
+    return (
+        <div className='background'>
+            <Header></Header>
+            <div className='text-part'>
+                <h1>{cours}</h1>
+                <p>{texte}</p>
+            </div>               
+            <div className='question-part'>
+                <ul>
+                {questions.map(question => <li>{question}</li>)}
+                </ul>
+            </div>
+        </div>
+    )
+} export default Study;
