c01c6b1c9c35e54135c711eeeeba3e5d2c1f648d
MarieEveille <eveille.marie29@gmail.com>
2024-02-05 16:39:53 +0100

    [ADD] Page principale des quizzs

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import TestHeader from './test/TestHeader'
 import SecurePage from './secure_page/securePage';
 import Profile from './profile/Profile';
 import Study from './STUDY/Study';
+import Quizz_principale from './quizz/Quizz_principale';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Routes> {/* Utilisez la composante "Routes" au lieu de "Switch" */}
         <Route path="/connexion" element={<Connexion />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/quizz" element={/*<PrivateRoute> */<Quizz_principale /> /*</PrivateRoute>*/} />
         <Route path="/carte_mental" element={<PrivateRoute><Carte_mental /></PrivateRoute>} />
         <Route path="/etude" element={/* <PrivateRoute> */<Study/>/* </PrivateRoute> */}/>
         <Route path="/secure_page" element={<PrivateRoute><SecurePage /></PrivateRoute>} />
---
+++ src/quizz/Quizz_principale.js
@@ -0,0 +1,77 @@
+//dependances
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import "@fontsource/nanum-pen-script";
+import Header from '../composent/Header.js';
+import heart from './img/heart.png';
+import './Quizz.css';
+
+function Quizz_principale () {
+  const navigate = useNavigate();
+
+  return (
+    <div className='background_quizz_principale'>
+      <Header></Header>
+        <div className='base-container_quizz_principale'>
+          <h1>Quizz de l'UE</h1>
+          <div className='container_quizzs'>
+            <div className='container_quizz'>
+              <div id='quizz_num' className='item_quizz'>
+                <p>Quizz 1</p>
+              </div>
+              <div id='quizz_sujet' className='theme_quizz'>
+                <p>Les bases de donnees</p>
+              </div>
+              <div id='quizz_like' className='quizz_like'>
+                <p>2 </p>
+                <img className='img_coeur' src={heart} alt='like'/>
+              </div>
+              <button onClick={() => navigate('/')} className='btn_quizz'>Commencer</button>
+            </div>
+            <div className='container_quizz'>
+              <div id='quizz_num' className='item_quizz'>
+                <p>Quizz 1</p>
+              </div>
+              <div id='quizz_sujet' className='theme_quizz'>
+                <p>Les bases de donnees</p>
+              </div>
+              <div id='quizz_like' className='quizz_like'>
+                <p>2 </p>
+                <img className='img_coeur' src={heart} alt='like'/>
+              </div>
+              <button onClick={() => navigate('/')} className='btn_quizz'>Commencer</button>
+            </div>
+            <div className='container_quizz'>
+              <div id='quizz_num' className='item_quizz'>
+                <p>Quizz 1</p>
+              </div>
+              <div id='quizz_sujet' className='theme_quizz'>
+                <p>Les bases de donnees</p>
+              </div>
+              <div id='quizz_like' className='quizz_like'>
+                <p>2 </p>
+                <img className='img_coeur' src={heart} alt='like'/>
+              </div>
+              <button onClick={() => navigate('/')} className='btn_quizz'>Commencer</button>
+            </div>
+            <div className='container_quizz'>
+              <div id='quizz_num' className='item_quizz'>
+                <p>Quizz 1</p>
+              </div>
+              <div id='quizz_sujet' className='theme_quizz'>
+                <p>Les bases de donnees</p>
+              </div>
+              <div id='quizz_like' className='quizz_like'>
+                <p>2 </p>
+                <img className='img_coeur' src={heart} alt='like'/>
+              </div>
+              <button onClick={() => navigate('/')} className='btn_quizz'>Commencer</button>
+            </div>
+            
+            </div>
+        </div>
+    </div>
+  );
+  
+} export default Quizz_principale;
