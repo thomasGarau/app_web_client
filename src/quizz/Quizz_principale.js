851a8f678e0b74474a693b0df1d5ab67489c275b
MarieEveille <eveille.marie29@gmail.com>
2024-02-12 08:41:04 +0100

    blabkla ilage

--- src/quizz/Quizz_principale.js
+++ src/quizz/Quizz_principale.js
@@ -4,7 +4,8 @@ import { useNavigate } from 'react-router-dom';
 
 import "@fontsource/nanum-pen-script";
 import Header from '../composent/Header.js';
-import heart from './img/heart.png';
+import stars from './img/star_empty.png';
+import stars_yellow from './img/star_full.png';
 import './Quizz.css';
 
 function Quizz_principale () {
@@ -25,46 +26,7 @@ function Quizz_principale () {
               </div>
               <div id='quizz_like' className='quizz_like'>
                 <p>2 </p>
-                <img className='img_coeur' src={heart} alt='like'/>
-              </div>
-              <button onClick={() => navigate('/')} className='btn_quizz'>Commencer</button>
-            </div>
-            <div className='container_quizz'>
-              <div id='quizz_num' className='item_quizz'>
-                <p>Quizz 1</p>
-              </div>
-              <div id='quizz_sujet' className='theme_quizz'>
-                <p>Les bases de donnees</p>
-              </div>
-              <div id='quizz_like' className='quizz_like'>
-                <p>2 </p>
-                <img className='img_coeur' src={heart} alt='like'/>
-              </div>
-              <button onClick={() => navigate('/')} className='btn_quizz'>Commencer</button>
-            </div>
-            <div className='container_quizz'>
-              <div id='quizz_num' className='item_quizz'>
-                <p>Quizz 1</p>
-              </div>
-              <div id='quizz_sujet' className='theme_quizz'>
-                <p>Les bases de donnees</p>
-              </div>
-              <div id='quizz_like' className='quizz_like'>
-                <p>2 </p>
-                <img className='img_coeur' src={heart} alt='like'/>
-              </div>
-              <button onClick={() => navigate('/')} className='btn_quizz'>Commencer</button>
-            </div>
-            <div className='container_quizz'>
-              <div id='quizz_num' className='item_quizz'>
-                <p>Quizz 1</p>
-              </div>
-              <div id='quizz_sujet' className='theme_quizz'>
-                <p>Les bases de donnees</p>
-              </div>
-              <div id='quizz_like' className='quizz_like'>
-                <p>2 </p>
-                <img className='img_coeur' src={heart} alt='like'/>
+                <img className='img_coeur' src={stars_yellow} alt='like'/>
               </div>
               <button onClick={() => navigate('/')} className='btn_quizz'>Commencer</button>
             </div>
