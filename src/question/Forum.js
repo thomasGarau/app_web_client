3d3ed62f58a3e28efd91b95a57656ef23eb9a74f
Vietco311 <ant.colin311@gmail.com>
2024-03-26 16:35:05 +0100

    [UPDATE] Page forum opérationnelle

--- src/question/Forum.js
+++ src/question/Forum.js
@@ -1,71 +1,91 @@
 import React, { useState, useEffect } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-
-
+import Icon from '@mdi/react';
+import { mdiCommentPlus, mdiAlert } from '@mdi/js';
 import Header from '../composent/Header';
 import './Forum.css';
+import { Button, ButtonGroup, IconButton } from '@mui/material';
 
 function Forum() {
 
-    const { state } = useLocation()
+    const { search } = useLocation();
+    const params = new URLSearchParams(search);
+    const question = params.get('question');
+    const pseudo = params.get('pseudo');
+    const date_nav = params.get('date');
+    const etat = params.get('etat');
 
-    const [discussions, setDiscussions] = useState([{ question: state.question, username: state.username, date: state.date, etat: state.etat }])
+    // Vérifiez si les paramètres existent avant de les ajouter à l'état
+    const initialDiscussion = question && pseudo && date_nav && etat ? [[question, pseudo, date_nav, etat]] : [];
+
     const [overQuestion, setOverQuestion] = useState(false);
     const [message, setMessage] = useState('');
-
-    const today = new Date();
-    const month = today.getMonth() + 1;
-    const year = today.getFullYear();
-    const date = today.getDate();
-    const showtime = today.getHours() 
-    + ':' + today.getMinutes() 
-    + ":" + today.getSeconds();
-    const currentDate = showtime + " " + date + "/" + month + "/" + year;
-
+    const [discussions, setDiscussions] = useState(initialDiscussion);
+    const [lastQuestion, setLastQuestion] = useState()
+    console.log(discussions)
 
     const handleChange = (e) => {
         setMessage(e.target.value);
     }
 
     const submitMessage = (e) => {
+        const today = new Date();
+        const month = today.getMonth() + 1;
+        const year = today.getFullYear();
+        const date = today.getDate();
+        const showtime = today.getHours()
+            + ':' + today.getMinutes()
+            + ":" + today.getSeconds();
+        const currentDate = showtime + " " + date + "/" + month + "/" + year;
+
         if (e.key === 'Enter') {
-            const newMessage = {question: message, username: "username", date: currentDate, etat: "Ouvert"};
+            const newMessage = [e.target.value, "username", currentDate];
             setDiscussions(prevMessage => [...prevMessage, newMessage]);
-            setMessage('');
+
         }
     }
+
+    useEffect(() => {
+        setMessage('');
+    }, [discussions])
 
     return (
         <div className='background-forum'>
             <Header></Header>
             <div className='forum-container'>
                 <div className='title-container'>
-                    <h1 className='forum-title'>{state.question}</h1>
-                    <span className='title-date'>{state.date}</span>
+                    <h1 className='forum-title'>{discussions[0][0]}</h1>
+                    <span className='title-date'>{discussions[0][2]}</span>
                 </div>
-                {discussions.map(poste =>
-                    <div className="forum-question"
-                        key={poste.question}
-                        onMouseOver={() => setOverQuestion(true)}
-                        onMouseOut={() => setOverQuestion(false)}>
-                        <div className='question-head'>
-                            <span className='who-when'>{poste.username} - {poste.date}</span>
-                            <div className={overQuestion ? 'button-container-show' : 'button-container-hide'}>
-                                <button className='reaction-button'></button>
-                                <button className='reaction-button'></button>
+                <div className='question-container'>
+                    {discussions.map(poste =>
+                        <div className="forum-question"
+                            key={poste[0]}
+                            onMouseOver={() => setOverQuestion(true)}
+                            onMouseOut={() => setOverQuestion(false)}>
+                            <div className='question-head'>
+                                <span className='who-when'>{poste[1]} - {poste[2]}</span>
+                                {/* <ButtonGroup className={overQuestion ? 'button-container-show' : 'button-container-hide'}>
+                                    <IconButton variant='outlined'><Icon path={mdiCommentPlus} size={1} /></IconButton>
+                                    <IconButton variant='outlined'><Icon path={mdiAlert} size={1} /></IconButton>
+                                </ButtonGroup> */}
+                            </div>
+                            <div className={discussions.indexOf(poste) % 2 == 1 ? 'question blue' : 'question grey'}>
+                                <span className='question-text'>{poste[0]}</span>
                             </div>
                         </div>
-                        <div className='question'>
-                            <span className='question-text'>{poste.question}</span>
-                        </div>
-                    </div>
-                )}
-                <input
-                    className='input-forum'
-                    placeholder='Nouveau message...'
-                    onChange={handleChange}
-                    onKeyDown={submitMessage}
-                    value={message} />
+                    )}
+                </div>
+
+                <div className='comment-footer'>
+                    <input
+                        className='input-forum'
+                        placeholder='Nouveau message...'
+                        onChange={handleChange}
+                        onKeyDown={submitMessage}
+                        value={message} />
+                </div>
+
 
             </div>
         </div>
