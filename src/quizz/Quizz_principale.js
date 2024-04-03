1a4ee742f9e9a83bd4ff5b44849065284c893c0d
MarieEveille <eveille.marie29@gmail.com>
2024-04-03 19:31:58 +0200

    Desgin fini de quizz

--- src/quizz/Quizz_principale.js
+++ src/quizz/Quizz_principale.js
@@ -1,22 +1,35 @@
-//dependances
+// Dépendances
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import "@fontsource/nanum-pen-script";
 import Header from '../composent/Header.js';
-import stars from './img/star_empty.png';
 import stars_yellow from './img/star_full.png';
 import './Quizz.css';
 
 function Quizz_principale () {
   const navigate = useNavigate();
+  const [activeTab, setActiveTab] = useState('prof');
+
+  const getTabStyle = (tabName) => ({
+    textDecoration: activeTab === tabName ? 'underline' : 'none',
+    color: activeTab === tabName ? 'black' : 'grey',
+    cursor: 'pointer'
+  });
 
   return (
     <div className='background_quizz_principale'>
       <Header></Header>
-        <div className='base-container_quizz_principale'>
-          <h1 className='quizz-title'>Quizz de l'UE</h1>
-          <div className='container_quizzs'>
+      <div className='base-container_quizz_principale'>
+        <h1 className='quizz-title'>Quizz de l'UE</h1>
+        <div className='quizz_type'>
+          <h2 className='Nom_Quizz' style={getTabStyle('prof')} onClick={() => setActiveTab('prof')}> Quizz Prof</h2>
+          <h2 className='Nom_Quizz' style={getTabStyle('eleve')} onClick={() => setActiveTab('eleve')}> Quizz Etudiant</h2>
+        </div>
+
+        <div className='container_quizzs'>
+          {activeTab === 'prof' && (
+            // Contenu de l'onglet 'Quizz Prof'
             <div className='container_quizz'>
               <div id='quizz_num' className='item_quizz'>
                 <p>Quizz 1</p>
@@ -30,9 +43,25 @@ function Quizz_principale () {
               </div>
               <button onClick={() => navigate('/')} className='btn_quizz'>Commencer</button>
             </div>
-            
+          )}
+          {activeTab === 'eleve' && (
+            <div className='container_quizz'>
+            <div id='quizz_num' className='item_quizz'>
+              <p>Quizz 2</p>
             </div>
+            <div id='quizz_sujet' className='theme_quizz'>
+              <p>Les maths miam</p>
+            </div>
+            <div id='quizz_like' className='quizz_like'>
+              <p>4 </p>
+              <img className='img_coeur' src={stars_yellow} alt='like'/>
+            </div>
+            <button onClick={() => navigate('/')} className='btn_quizz'>Commencer</button>
+          </div>
+          )}
+          <button onClick={() => navigate('/createQuizz')} className='btn_quizz_creer'>Creer un quizz</button>
         </div>
+      </div>
     </div>
   );
   
