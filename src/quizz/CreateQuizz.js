bd6d638ae56c1ffdb160757fd27f50de5fbed35f
Anthony Colin <ant.colin311@gmail.com>
2024-04-03 20:59:48 +0200

    [UPDATE]CreateQuizz design complété sans la logique

--- src/quizz/CreateQuizz.js
+++ src/quizz/CreateQuizz.js
@@ -1,4 +1,4 @@
-import { Box, Button, Input, Typography } from "@mui/material";
+import { Box, Button, Checkbox, Input, Typography } from "@mui/material";
 import IconButton from '@mui/joy/IconButton';
 import AddIcon from '@mui/icons-material/Add';
 import Header from "../composent/Header";
@@ -10,10 +10,12 @@ function CreateQuizz() {
     const [questions, setQuestions] = useState([{ title: "Premiere question", reponses: ["un truc", "un machin", "une chose"] }]);
     const [selected, setSelected] = useState(questions[0]);
 
+    const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
     return (
-        <div style={{ height: "100vh"}}>
+        <div style={{ height: "100vh" }}>
             <Header></Header>
-            <div className="container-create-quizz" style={{ display:"flex"}}>
+            <div className="container-create-quizz" style={{ display: "flex" }}>
                 <nav className="nav-question">
                     <Input id="title-input" placeholder="Titre du quizz" variant="plain" required></Input>
                     {questions.map(question => (
@@ -32,6 +34,25 @@ function CreateQuizz() {
 
                 </nav>
                 <div className="background-create-quizz">
+                    <div className="answer-container">
+                        <Typography style={{ color: "white", fontFamily: "Nanum Pen Script", fontSize: "xx-large", marginTop: "10px" }}>{selected.title}</Typography>
+                        <div style={{ display: "flex", flexDirection: "column", width:"70%" }}>
+                            {selected.reponses.map((reponse, index) => (
+                                <div key={index} style={{ display: "flex", alignItems: "center", margin: "15px 0px" }}>
+                                    <Input
+                                        placeholder={"Reponse " + (index + 1)}
+                                        style={{ color: "black", fontFamily: "Nanum Pen Script", fontSize: "x-large", backgroundColor: "white", padding: "5px", borderRadius: "10px", width: "100%" }}
+                                        value={reponse}
+                                    />
+                                    <Checkbox {...label} style={{ color:"white"}}/>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <Button
+                        style={{ color: "white", fontFamily: "Nanum Pen Script", fontSize: "large", backgroundColor: "#133D56", padding: "10px", borderRadius: "20px", position: "absolute", bottom:"5%", right:"5%" }}>
+                        Valider le quizz
+                    </Button>
                 </div>
             </div>
         </div>
