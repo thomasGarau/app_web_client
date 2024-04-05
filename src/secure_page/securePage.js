646325ed68e0c3de7c93ac2f4b5c0739468cbb89
Anthony Colin <ant.colin311@gmail.com>
2024-04-05 16:09:27 +0200

    [UPDATE]Espace Etudiant responsive pour téléphone

--- src/secure_page/securePage.js
+++ src/secure_page/securePage.js
@@ -13,7 +13,8 @@ function SecurePage() {
   const [isSecure, setIsSecure] = useState(null);
   const [listUE, setListUE] = useState([
     { id: 0, icon: "python.png", prof: { nom: "Bisgambiglia", prenom: "Paul-Antoine" }, ue: "Python" },
-    { id: 0, icon: "github.png", prof: { nom: "Filippi", prenom: "Jean-Baptiste" }, ue: "Github" }])
+    { id: 1, icon: "github.png", prof: { nom: "Filippi", prenom: "Jean-Baptiste" }, ue: "Github" },
+    { id: 2, icon: "github.png", prof: { nom: "Filippi", prenom: "Jean-Baptiste" }, ue: "Github" }])
   const navigate = useNavigate();
   const [searchQuery, setSearchQuery] = useState("");
 
@@ -58,31 +59,49 @@ function SecurePage() {
   );
 
   return (
-    <div className='style_background'>
+    <div className='style_background_esp_ele'>
       <Header></Header>
       <div className='container2_style'>
         <h1 style={{ fontSize: "xxx-large" }}>Espace Eleve</h1>
-        <div style={{ display: "flex", width: "100%" }}>
-          <div style={{ display: "flex", width: "50%", height: "80%", backgroundColor: "#133D56", borderRadius: "20px", flexDirection: "column" }}>
+        <div className="sub_container_ue_j" style={{ display: "flex", width: "90%", justifyContent: "space-between" }}>
+          <div className="sub_container_ue" style={{
+            backgroundColor: "#133D56",
+            borderRadius: "20px",
+            marginBottom: "20px",
+            display: "flex",
+            flexDirection: "column",
+          }}>
             <h2 style={{ marginLeft: "20px", fontSize: "xx-large", color: "#F5F5F5" }}>Liste d'UE</h2>
             <TextField
               onChange={(e) => setSearchQuery(e.target.value)}
-              style={{ width: "90%", alignSelf: "center", borderRadius: "50px" }}
               sx={{
-                input: { color: '#f5f5f5', fontFamily: "Nanum Pen Script", fontSize: "x-large" }, '& .MuiOutlinedInput-root': {
+                width: "90%",
+                alignSelf: "center",
+                borderRadius: "50px",
+                color: '#f5f5f5',
+                fontFamily: "Nanum Pen Script",
+                fontSize: "x-large",
+                '& .MuiOutlinedInput-root': {
                   borderRadius: "50px",
-                }, border: 3,
+                },
+                border: 3,
               }}
               variant="outlined"
               InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">
-                    <IconButton ><SearchIcon sx={{ width: 40, height: 40 }} /></IconButton>
+                    <IconButton><SearchIcon sx={{ width: 40, height: 40 }} /></IconButton>
                   </InputAdornment>
                 ),
               }}
             />
-            <List>
+            <List sx={{
+              width: '100%',
+              position: 'relative',
+              overflow: 'auto',
+              maxHeight: 300,
+              '& ul': { padding: 0 },
+            }}>
               {filteredListUE.map(ue => (
                 <ListItem style={{ marginLeft: "10px" }} key={ue.id}>
                   <ListItemAvatar>
@@ -101,18 +120,17 @@ function SecurePage() {
                 </ListItem>
               ))}
             </List>
-
           </div>
           <div>
             <h2 style={{ fontSize: "xx-large" }}>Methode des j</h2>
             <>{/* Il y aura un calendrier ici !!! */}</>
           </div>
-
         </div>
         {isSecure}
         <Button variant="contained" onClick={handleDisconnection}>Deconnexion</Button>
       </div>
     </div>
+
   );
 }
 
