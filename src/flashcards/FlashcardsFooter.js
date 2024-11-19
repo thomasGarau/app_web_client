f4545ddfda7c7ad0d013f2686dfaec74449e0516
Anthony Colin <ant.colin311@gmail.com>
2024-11-19 15:32:11 +0100

    [FIX]Creation of flashcards from FlashcardsDisplayer.js fixed

--- src/flashcards/FlashcardsFooter.js
+++ src/flashcards/FlashcardsFooter.js
@@ -2,7 +2,7 @@ import { Box } from '@mui/material';
 import StyledButton from '../composent/StyledBouton';
 import React from 'react';
 
-export default function FlashcardsFooter({ onNewClick, onEditClick, onDelClick, delMode, editingMode }) {
+export default function FlashcardsFooter({ onNewClick, onEditClick, onDelClick, currentMode }) {
     return (
         <Box
             sx={{
@@ -13,14 +13,22 @@ export default function FlashcardsFooter({ onNewClick, onEditClick, onDelClick, 
                 justifyContent: { xs: 'center', sm: 'space-evenly' },
                 flexDirection: { xs: 'column', sm: 'row' },
                 backgroundColor: '#093146',
-                height: { xs: '100px', sm: '150px'},
+                height: { xs: '100px', sm: '150px' },
                 alignItems: 'center',
                 gap: '10px',
             }}
         >
-            <StyledButton content={"Nouvelle flashcards"} color={"primary"} onClick={onNewClick} />
-            <StyledButton content={"Supprimer flashcards"} color={delMode ? "#cc0000" : "primary"} onClick={onDelClick} />
-            <StyledButton content={editingMode ? "Editer flashcards" : "Consulter flashcards"} color={editingMode ?  "secondary" : "primary"} onClick={onEditClick} />
+            <StyledButton content={"Nouvelle flashcards"} onClick={onNewClick} color={currentMode === 'creating' ? "green" : "primary" } />
+            <StyledButton content={"Supprimer flashcards"} color={currentMode === 'deleting' ? "#cc0000" : "primary"} onClick={onDelClick} />
+            <StyledButton
+                color={currentMode === "editing" ? "secondary" : "primary"}
+                content={currentMode === "editing" ? "Editer flashcards" : "Consulter flashcards"}
+                onClick={() => {
+                    console.log("Edit button clicked");
+                    onEditClick();
+                }}
+            />
+
         </Box>
     );
 }
