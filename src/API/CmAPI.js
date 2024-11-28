dae3ad504d3bc740458d533f5c5e072228c76bae
MarieEveille <eveille.marie29@gmail.com>
2024-11-28 19:00:29 +0100

    [UPDATE] Connexion commencé entre le front et back

---
+++ src/API/CmAPI.js
@@ -0,0 +1,119 @@
+import api from '../config/axiosConfig';
+
+export const createCM = async (data, path) => {
+    try {
+        const formData = new FormData();
+        formData.append('data', data);
+        formData.append('path', path);
+
+        const response = await api.post(`/carte-mentale/create-cm`, formData, {
+            headers: {
+                'Content-Type':'multipart/form-data'
+            }
+        });
+
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+
+
+export const getCMUser = async (id_chap) => {
+    try {
+        const body = {
+            chapitre: id_chap
+        };
+        const response = await api.post(`/carte-mentale/user-cm`, body);
+        return response.data;
+    }
+    catch (error) {
+        throw error;
+    }
+};
+
+
+export const getCMInfo = async (id_CM) => {
+    try {
+        const body = {
+            cm : id_CM
+        };
+        const response = await api.post(`/carte-mentale/cm-info`, body);
+        return response.data;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+
+export const getCmDetails = async (id_CM) => {
+    try {
+        const body = {
+            cm : id_CM
+        };
+        const response = await api.post(`/carte-mentale/cm-details`, body);
+        return response.data;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+
+
+export const getAllCM = async (id_chap) => {
+    try {
+        const body = {
+            chapitre: id_chap
+        };
+        const response = await api.post(`/carte-mentale/all-cm-chapter`, body);
+        return response.data;
+    }
+    catch (error) {
+        throw error;
+    }
+};
+
+
+export const addCmToCollection = async (id_cm) => {
+    try {
+        const body = {
+            cm: id_cm
+        };
+        const response = await api.post(`/carte-mentale/add-cm-to-collection`, body);
+        return response.data;
+    }
+    catch (error) {
+        throw error;
+    }
+};
+
+export const deleteCmFromCollection = async (id_cm) => {
+    try {
+        const response = await api.delete(`/carte-mentale/remove-cm-from-collection`,  {
+            data: {
+                cm : id_cm
+            }
+        });
+        return response.data;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+
+
+export const deleteCM = async (id_cm) => {
+    try {
+        const response = await api.delete(`/carte-mentale/delete-cm`, {
+            data: {
+                cm : id_cm
+            }
+    });
+        return response.data;
+
+    }
+    catch (error) {
+        throw error;
+    }
+};
--- src/carte_mental/Voir_CM.js
+++ src/carte_mental/Voir_CM.js
@@ -4,71 +4,77 @@ import { useParams, useNavigate } from "react-router-dom";
 import StyledButton from '../composent/StyledBouton';
 import { getTokenAndRole } from '../services/Cookie';
 import {jwtDecode} from 'jwt-decode';
+import { getCMInfo, addCmToCollection, deleteCmFromCollection, deleteCM } from '../API/CmAPI';
 
 
 function Voir_CM() {
-    const {id_CM} = useParams();
+    const {id_chap, id_CM} = useParams();
     const [title, setTitle] = useState('');
     const [isCollection, setIsCollection] = useState(null);
     const [image, setImage] = useState(null);
-    const [imgUrl, setImgUrl] = useState('https://res.cloudinary.com/dcyjnrgxh/image/upload/v1715891093/image-ue/on8waw6k2tptixnavlbb.png');
     const navigate = useNavigate();
-    const [id_utilisateur, setId_utilisateur] = useState(undefined);
+    const [id_user, setid_user] = useState(undefined);
     const [id_createur, setId_createur] = useState(undefined);
+    const [is_proprio, setis_proprio] = useState(undefined);
 
 
 
     async function fetchData(params) {
         const { token } = await getTokenAndRole();
         const decodedToken = jwtDecode(token);
-        setId_utilisateur(decodedToken.id_etudiant);
-        console.log('id_utilisateur:', id_utilisateur);
+        setid_user(decodedToken.id_etudiant);
     }
 
 
-    useEffect(() => {
-        fetchData();
-        console.log('id_utilisateur:', id_utilisateur);
-        fetch(`${process.env.PUBLIC_URL}/mindmap1.json`)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Erreur lors du chargement du fichier JSON');
-                }
-                return response.json();
-            })
-            .then((data) => {
-                // Accéder à la section "details" du JSON
-                const details = data.details || {};
 
-                // Mettre à jour le titre depuis "details.title"
-                setId_createur(data.id_createur);
-                setTitle(details.title || 'Carte Mentale');
-                setIsCollection(data.is_collection || false);
-                setImage(details.image || `${process.env.PUBLIC_URL}/cartementale.png`);
-            })
-            .catch((error) => {
-                console.error('Erreur:', error);
-            });
-    }, []);
+useEffect(() => {
+
+    if(id_CM === undefined) {
+        navigate(`/carte_mentale/${id_chap}`);
+    }
+    const fetchAllData = async () => {
+        await fetchData(); 
+        const data = await getCMInfo(id_CM);
+        console.log(data);
+        if (!data || (typeof data === "object" && Object.keys(data).length === 0)) {
+            navigate(`/carte_mentale/${id_chap}`);
+            return;
+        }
+        setId_createur(data.id_utilisateur);
+        setTitle(data.titre || 'Carte Mentale');
+        setIsCollection(data.is_collection || false);
+        setImage(data.url || '');
+        setis_proprio(id_user === id_createur);
+    };
+
+    fetchAllData().catch((error) => console.error('Erreur:', error));
+}, [is_proprio, id_CM, id_user]);
+
 
     function navEditCM() {
         navigate(`/edit_carte_mentale/${id_CM}`);
     }
 
-    function handleToggleCollection() {
-        setIsCollection(!isCollection); // Basculer l'état
+    async function handleToggleCollection() {
+        if(isCollection) {
+            await deleteCmFromCollection(id_CM);
+        }
+        else {
+            await addCmToCollection(id_CM);
+        }
+        setIsCollection(!isCollection); 
     }
 
-    function handleDeleteCM() {
+    async function handleDeleteCM() {
         if (window.confirm('Êtes-vous sûr de vouloir supprimer cette carte mentale ?')) {
             // Remplacez par votre logique de suppression (appel API, etc.)
-            console.log(`Carte mentale avec ID ${id_CM} supprimée.`);
-            navigate('/carte_mental'); // Redirigez après suppression
+            await deleteCM(id_CM);
+            navigate(`/carte_mentale/${id_chap}`); 
         }
     }
 
     function handleDownloadImg() {
-        const imageUrl = imgUrl; // Chemin local ou distant
+        const imageUrl = image; 
         fetch(imageUrl).then((response) => {
                 if (!response.ok) {
                     throw new Error('Erreur lors de la récupération de l\'image');
@@ -88,22 +94,31 @@ function Voir_CM() {
             .catch((error) => console.error('Erreur lors du téléchargement de l\'image:', error));
     }
     
+    function handleRetour()  {
+        navigate('/carte_mentale/' + id_chap);
+      }
     
-
 
 
     return (
         <div className='container_voir_cm'>
             <div className='container-voir-cm-boutons'>
-            {id_utilisateur === id_createur && (
+
+            <StyledButton
+                    content={"Retour"}
+                    color={"primary"}
+                    onClick={handleRetour}
+                    width={"200"}
+                />
                 <StyledButton
                         content={"Modifier la carte mentale"}
                         color={"primary"}
                         onClick={navEditCM}
                         width={199}
                         fontSize={1}
+                        isHidden={!is_proprio}
                 />
-            )}
+                
                 <StyledButton
                     content={"Télécharger la carte mentale"}
                     color={"primary"}
@@ -113,33 +128,36 @@ function Voir_CM() {
                     
                 />
 
-            {id_utilisateur !== id_createur && (
-                <StyledButton
-                    content={
-                        isCollection
-                            ? "Supprimer de la collection"
-                            : "Ajouter à la collection"
-                    }
-                    color={"primary"}
-                    onClick={handleToggleCollection}
-                    width={200}
-                    fontSize={1}
-                />
-            )}
-                {id_utilisateur === id_createur && (
+    
+                    <StyledButton
+                        content={
+                            isCollection
+                                ? "Supprimer de la collection"
+                                : "Ajouter à la collection"
+                        }
+                        color={"primary"}
+                        onClick={handleToggleCollection}
+                        width={200}
+                        fontSize={1}
+                        isHidden={is_proprio}
+                    />
+    
+
+    
                     <StyledButton
                         content={"Supprimer la carte mentale"}
                         color={"primary"}
                         onClick={handleDeleteCM}
                         width={200}
                         fontSize={1}
+                        isHidden={!is_proprio}
                     />
-                )}
+                
                 
             </div>
             <div className='container-voir-cm-image'>
                 <h1>{title}</h1>
-                <img src='https://coccibel.fr/wp-content/uploads/2023/09/carteMentale-couv.png' alt="Carte mentale" style={{ width: '50%'}} />
+                <img src={image} alt="Carte mentale" style={{ height: '60%'}} />
             </div>
 
 
